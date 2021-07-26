4e25da590ffe437808041066fe645f1a58495f1e
peirato <peirato3@163.com>
2021-07-26 18:34:25 +0800

    class02.Code03对数器开发中

--- algo-class/class02/Code03_KM.go
+++ algo-class/class02/Code03_KM.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 一个数组，一种数出现了k次，其他都出现了m次，m>1，m>k,找到k
 
-func onlyKTimes(arr []int, k int, m int) {
+func onlyKTimes(arr []int, k int, m int) int{
 
 	var t [32]int
 
@@ -26,8 +26,6 @@ func onlyKTimes(arr []int, k int, m int) {
 		}
 	}
 
-	fmt.Println(t)
-
 	x := 0
 
 	for i := range arr {
@@ -37,12 +35,39 @@ func onlyKTimes(arr []int, k int, m int) {
 		}
 	}
 
-	fmt.Println(x)
+	return x
 }
 
-// TODO 实现一个对数器
+func comparators() int{
+	return 0
+}
+
+maxNum := 100
+maxTime := 100
+func genArray() []int{
+	k,m = genKM()
+}
+
+func genKM()int,int{
+
+	kNum := randomInt(maxNum-1)
+	
+	var mNum int
+	
+	for{
+		mNum = randomInt(maxNum)
+		if nMum > kNum {
+			return k,m
+		}
+	}
+}
+
+
+func randomInt(max int) int {
+        a, _ := rand.Int(rand.Reader, big.NewInt(int64(max)))
+        return int(a.Int64())
+}
 
 func main() {
 	onlyKTimes([]int{3, 1, 1, 4, 4}, 1, 2)
-
 }
